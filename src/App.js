ae5aa2e3a66faefe1ce106c4b06a1e04f3468ff6
Tim Bedsole <tim@Tims-MacBook-Air.local>
2023-05-05 23:04:25 -0400

    Added input field and filtering logic to search names

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ class App extends Component {
 
     this.state = {
       monsters: [],
+      searchField: '',
     }
   }
 
@@ -24,9 +25,26 @@ class App extends Component {
   }
   
   render() {
+
+    const filteredMonsters = this.state.monsters.filter((monster) => {
+      return monster.name.toLowerCase().includes(this.state.searchField)
+    })
+    
     return (
       <div className="App">
-        
+        <input className='search-box' type='search' placeholder='search monsters' onChange={(event) => {
+          const searchField = event.target.value.toLowerCase();
+          this.setState(() => {
+            return { searchField };
+          })
+        }} />
+        {filteredMonsters.map((monster) => {
+          return (
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+          )
+        })}
       </div>
     );
   }
